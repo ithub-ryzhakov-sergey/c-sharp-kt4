e3ff36a3c9c17260557080144610cb4fee7cafa7
user1 <user1@DESKTOP-ENRJJP4>
2025-09-20 16:25:49 +0300

    task 4

--- App/Topics/CheckedUnchecked/T2_1_SafeAdd/ArithmeticUtils.cs
+++ App/Topics/CheckedUnchecked/T2_1_SafeAdd/ArithmeticUtils.cs
@@ -5,7 +5,32 @@ public static class ArithmeticUtils
     // Сложение с управлением переполнением. Требуется реализовать 3 стратегии.
     public static int SafeAdd(int a, int b, OverflowStrategy strategy)
     {
-        // Требуется реализация студентом.
-        throw new NotImplementedException();
+        if (strategy == OverflowStrategy.Checked)
+        {
+            return checked(a + b);
+        }
+
+        if (strategy == OverflowStrategy.UncheckedWrap)
+        {
+            return unchecked(a + b);
+        }
+
+        if (strategy == OverflowStrategy.Saturate)
+        {
+            long result = (long)a + (long)b;
+
+            if(result > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+            if (result < int.MinValue)
+            {
+                return int.MinValue;
+            }
+
+            return (int)result;
+        }
+
+        throw new ArgumentException("Ошибка");
     }
 }
