48820609b01c5f01ed9fd4806bcbc00e06856f3f
user1 <user1@DESKTOP-ENRJJP4>
2025-09-20 15:14:49 +0300

    task2

--- App/Topics/ThrowFinally/T1_2_FinallyPitfalls/FinallyPitfalls.cs
+++ App/Topics/ThrowFinally/T1_2_FinallyPitfalls/FinallyPitfalls.cs
@@ -8,8 +8,14 @@ public static class FinallyPitfalls
 {
     public static int ReturnWithFinallyModification(int input)
     {
-        // Требуется реализация студентом.
-        // Подсказка: используйте локальную переменную, меняйте её в finally, возвращайте в try.
-        throw new System.NotImplementedException();
+        try
+        {
+            return input;
+        }
+
+        finally
+        {
+            input++;
+        }
     }
 }
