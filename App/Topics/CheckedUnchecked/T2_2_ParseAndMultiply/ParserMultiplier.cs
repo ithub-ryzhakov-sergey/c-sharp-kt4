45bcd0ba473a5e12f245115a00768b01e3482bee
user1 <user1@DESKTOP-D0T1IQ4>
2025-10-25 14:28:41 +0300

    `1

--- App/Topics/CheckedUnchecked/T2_2_ParseAndMultiply/ParserMultiplier.cs
+++ App/Topics/CheckedUnchecked/T2_2_ParseAndMultiply/ParserMultiplier.cs
@@ -5,7 +5,20 @@ public static class ParserMultiplier
     // Парсит две строки и перемножает. При useChecked=true переполнение должно приводить к OverflowException
     public static int ParseAndMultiply(string a, string b, bool useChecked)
     {
-        // Требуется реализация студентом.
-        throw new NotImplementedException();
+        if (String.IsNullOrEmpty(a) || String.IsNullOrEmpty(b) || a.Trim()== "" || b.Trim()== "")
+        {
+            throw new ArgumentException();
+        }
+        int _a;
+        int _b;
+        if (!int.TryParse(a, out _a) || !int.TryParse(b, out _b))
+        {
+            throw new FormatException();
+        }
+        if ((long)_a * _b > int.MaxValue  && useChecked)
+        {
+            throw new OverflowException();
+        }
+        return _a * _b;
     }
 }
